482c0e6f11076c2bd79f4c0dcc0a889d11589c79
Nikhil poojari <poojarinikhil01@gmail.com>
2022-08-08 23:23:53 +0530

    eventlistnes

--- style.js
+++ style.js
@@ -1,3 +1,10 @@
-function buttonclick(){
-    console.log('button click')
-}
+// function buttonclick(){
+//     console.log('button click')
+// }
+let inputbtn = document.getElementById("input-btn")
+
+inputbtn.addEventListener("click", function(){
+    alert("button has been clicked using event listener")
+    document.getElementById("input-btn").style.backgroundColor = 'red';
+});
+
