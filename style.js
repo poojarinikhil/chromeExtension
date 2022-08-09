ba6b0c05b63173ac034d7551f4378ff0fb05c8ce
Nikhil poojari <poojarinikhil01@gmail.com>
2022-08-09 15:31:31 +0530

    pushing into array

--- style.js
+++ style.js
@@ -2,9 +2,12 @@
 //     console.log('button click')
 // }
 let inputbtn = document.getElementById("input-btn")
-
-inputbtn.addEventListener("click", function(){
+let myLeads = []
+const inputEl = document.getElementById("input-el") 
+inputbtn.addEventListener("click", myfunction);
+function myfunction(){
     alert("button has been clicked using event listener")
-    document.getElementById("input-btn").style.backgroundColor = 'red';
-});
-
+    document.getElementById("input-btn").style.backgroundColor = 'cyan';
+    myLeads.push("www.awesomlead.com")
+    console.log(myLeads);
+}
